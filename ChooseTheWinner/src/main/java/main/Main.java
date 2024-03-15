1f65f669870d453112d7f21b3fda178d6f8ec9af
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2024-03-15 16:49:28 +0400

    LoginMenu and Menu added

--- ChooseTheWinner/src/main/java/main/Main.java
+++ ChooseTheWinner/src/main/java/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import java.util.Scanner;
+import util.MenuUtil;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        MenuUtil.loginMenu();
     }
 }
---
+++ ChooseTheWinner/src/main/java/util/MenuUtil.java
@@ -0,0 +1,74 @@
+package util;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MenuUtil {
+
+    public static void loginMenu() {
+        int wrongLogin = 0;
+        boolean loop = true;
+
+        while (loop) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Sign in!");
+            System.out.print("Username: ");
+            String username = sc.next();
+            System.out.print("Password: ");
+            String password = sc.next();
+
+            if (username.equals("Admin") && password.equals("12345")) {
+                System.out.println("Succesfuly login!!!");
+                menu();
+                loop = false;
+            } else {
+                System.out.println("Username or password is wrong!");
+                wrongLogin++;
+            }
+
+            if (wrongLogin > 2) {
+                System.out.println("You banned!!!");
+                loop = false;
+            }
+        }
+    }
+
+    public static void menu() {
+        boolean loop = true;
+
+        while (loop) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("1. Register racers");
+            System.out.println("2. Start race");
+            System.out.println("3. Logout");
+            System.out.println("4. Exit");
+
+            try {
+                System.out.print("Select: ");
+                int input = sc.nextInt();
+
+                switch (input) {
+                    case 1:
+                        
+                        break;
+                    case 2:
+                        
+                        break;
+                    case 3:
+                        
+                        break;
+                    case 4:
+                        
+                        break;
+                    default:
+                        System.out.println("You must enter true number!!!");
+                        break;
+                        
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("You must enter a number!!!");
+            }
+
+        }
+    }
+}
